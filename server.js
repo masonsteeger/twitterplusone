4850cd5b1bfb834133b76b42044e7aa8579c7f31
masonsteeger94 <masonsteeger@gmail.com>
2020-10-23 23:05:32 -0500

    test

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, 'client/build')))
-}
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 
 //ROUTES
 
